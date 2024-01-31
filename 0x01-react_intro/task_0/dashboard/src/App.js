3f8ff817ef327a273996b1c3b4d90c23ae1f6dae
Debby696 <idowuuopeyemi696@gmail.com>
2024-01-31 15:10:33 +0100

    react intro

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '@babel/plugin-proposal-private-property-in-object';
+import './App.css';
+import HolbertonLogo from './dashboard/holberton-logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={HolbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
