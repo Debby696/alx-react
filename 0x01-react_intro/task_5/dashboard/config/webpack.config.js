d7f7c90c4f6b11c6aa7da10dc7b806e91c7f09b7
“Debby696“ <idowuopeyemi696@gmail.com>
2024-02-09 16:44:49 +0100

    Reorganize the files

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, 'dist'),
     compress: true,
     port: 8080,
     hot: true,
